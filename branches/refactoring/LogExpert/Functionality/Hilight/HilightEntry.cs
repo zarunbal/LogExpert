79b68825246c022e3354ebd0fb8eea8785084689
Zarunbal_cp <SND\Zarunbal_cp>
2015-04-17 22:09:06 +0000

    splitted hilight file minor refactoring

--- branches/refactoring/LogExpert/Functionality/Hilight/HilightEntry.cs
+++ branches/refactoring/LogExpert/Functionality/Hilight/HilightEntry.cs
@@ -8,62 +8,27 @@ using System.Text.RegularExpressions;
 namespace LogExpert
 {
 	[Serializable]
-	public class ActionEntry
-	{
-		public string pluginName;
-		public string actionParam;
-
-		public ActionEntry Copy()
-		{
-			ActionEntry e = new ActionEntry();
-			e.pluginName = pluginName;
-			e.actionParam = actionParam;
-			return e;
-		}
-	}
-
-	[Serializable]
 	public class HilightEntry
 	{
+		#region Fields
+		
 		private string searchText = "";
 		private bool isCaseSensitive;
 		[NonSerialized]
-		private Regex regex = null;
-
+		private Regex _regex = null;
+		
 		[NonSerialized]
-		private bool isSearchHit;   // highlightes search result
+		private bool _isSearchHit;   // highlightes search result 
+		
+		#endregion
 
-		public bool IsStopTail { get; set; }
-
-		public bool IsSetBookmark { get; set; }
-
-		public bool IsRegEx { get; set; }
-
-		public bool IsCaseSensitive
-		{
-			get
-			{
-				return isCaseSensitive;
-			}
-			set
-			{
-				isCaseSensitive = value;
-				regex = null;
-			}
-		}
+		#region cTor
 
 		public HilightEntry(string searchText, Color fgColor, Color bgColor, bool isWordMatch)
 		{
 			SearchText = searchText;
 			ForegroundColor = fgColor;
 			BackgroundColor = bgColor;
-			IsRegEx = false;
-			isCaseSensitive = false;
-			IsLedSwitch = false;
-			IsStopTail = false;
-			IsSetBookmark = false;
-			IsActionEntry = false;
-			ActionEntry = null;
 			IsWordMatch = isWordMatch;
 		}
 
@@ -84,10 +49,33 @@ namespace LogExpert
 			IsWordMatch = isWordMatch;
 		}
 
+		#endregion
+
+		#region Properties
+		
+		public bool IsStopTail { get; set; }
+		
+		public bool IsSetBookmark { get; set; }
+		
+		public bool IsRegEx { get; set; }
+		
+		public bool IsCaseSensitive
+		{
+			get
+			{
+				return isCaseSensitive;
+			}
+			set
+			{
+				isCaseSensitive = value;
+				_regex = null;
+			}
+		}
+		
 		public Color ForegroundColor { get; set; }
-
+		
 		public Color BackgroundColor { get; set; }
-
+		
 		public string SearchText
 		{
 			get
@@ -97,70 +85,57 @@ namespace LogExpert
 			set
 			{
 				searchText = value;
-				regex = null;
+				_regex = null;
 			}
 		}
-
+		
 		public bool IsLedSwitch { get; set; }
-
+		
 		public ActionEntry ActionEntry { get; set; }
-
+		
 		public bool IsActionEntry { get; set; }
-
+		
 		public string BookmarkComment { get; set; }
-
+		
 		public Regex Regex
 		{
-			get 
+			get
 			{
-				if (regex == null)
+				if (_regex == null)
 				{
 					if (IsRegEx)
 					{
-						regex = new Regex(SearchText, IsCaseSensitive ? RegexOptions.None : RegexOptions.IgnoreCase);
+						_regex = new Regex(SearchText, IsCaseSensitive ? RegexOptions.None : RegexOptions.IgnoreCase);
 					}
 					else
 					{
-						regex = new Regex(Regex.Escape(SearchText), IsCaseSensitive ? RegexOptions.None : RegexOptions.IgnoreCase);
+						_regex = new Regex(Regex.Escape(SearchText), IsCaseSensitive ? RegexOptions.None : RegexOptions.IgnoreCase);
 					}
 				}
-				return regex; 
+				return _regex;
 			}
 		}
-
+		
 		public bool IsWordMatch { get; set; }
-
+		
 		public bool IsSearchHit
 		{
 			get
 			{
-				return isSearchHit;
+				return _isSearchHit;
 			}
 			set
 			{
-				isSearchHit = value;
+				_isSearchHit = value;
 			}
 		}
-
+		
 		public bool IsBold { get; set; }
-
+		
 		public bool NoBackground { get; set; }
-	}
+		
+		#endregion
+		
 
-	/// <summary>
-	/// Class for storing word-wise hilight matches. Used for colouring different matches on one line.
-	/// </summary>
-	public class HilightMatchEntry
-	{
-		public HilightEntry HilightEntry { get; set; }
-
-		public int StartPos { get; set; }
-
-		public int Length { get; set; }
-
-		public override String ToString()
-		{
-			return HilightEntry.SearchText + "/" + StartPos + "/" + Length;
-		}
 	}
 }
