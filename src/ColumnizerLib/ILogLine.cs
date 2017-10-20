87ed6cdd00d449c509a4968975011d2087e42a67
thomas <master.of.nothing@gmx.de>
2017-10-20 21:28:07 +0200

    added new interface ILogLine for loglines

---
+++ src/ColumnizerLib/ILogLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ColumnizerLib
+{
+    public interface ILogLine
+    {
+        string FullLine { get; }
+
+        int LineNumber { get; }
+
+        string DisplayLine { get;  }
+    }
+}
