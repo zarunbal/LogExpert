830488c8a905275ddb7a88b334313291fc53d0be
thomas <master.of.nothing@gmx.de>
2017-10-22 21:04:00 +0200

    reformat code

--- src/LogExpert/FilterPipe.cs
+++ src/LogExpert/FilterPipe.cs
@@ -6,186 +6,194 @@ using ColumnizerLib;
 
 namespace LogExpert
 {
-  public class FilterPipe
-  {
-    string fileName;
-    FilterParams filterParams;
-    IList<int> lastLinesHistoryList = new List<int>();
-    StreamWriter writer;
-    IList<int> lineMappingList = new List<int>();
-    LogWindow logWindow;      // the parent LogWindow
-    LogWindow ownLogWindow;    // own window
-    bool isStopped;
-
-    public bool IsStopped
+    public class FilterPipe
     {
-      get { return isStopped; }
-      set { isStopped = value; }
-    }
+        #region Fields
 
-    public FilterPipe(FilterParams filterParams, LogWindow logWindow)
-    {
-      this.filterParams = filterParams;
-      this.logWindow = logWindow;
-      this.IsStopped = false;
-      this.fileName = Path.GetTempFileName();
+        private IList<int> lineMappingList = new List<int>();
+        private StreamWriter writer;
 
-      Logger.logInfo("Created temp file: " + this.FileName);
-    }
+        #endregion
 
-    public void OpenFile()
-    {
-      FileStream fStream = new FileStream(this.fileName, FileMode.Append, FileAccess.Write, FileShare.Read);
-      this.writer = new StreamWriter(fStream, new UnicodeEncoding(false, false));
-    }
+        #region cTor
 
-    public void CloseFile()
-    {
-      if (this.writer != null)
-      {
-        this.writer.Close();
-        this.writer = null;
-      }
-    }
-
-    public bool WriteToPipe(ILogLine textLine, int orgLineNum)
-    {
-      try {
-        lock (this.FileName)
+        public FilterPipe(FilterParams filterParams, LogWindow logWindow)
         {
-          lock (this.lineMappingList)
-          {
+            this.FilterParams = filterParams;
+            this.LogWindow = logWindow;
+            this.IsStopped = false;
+            this.FileName = Path.GetTempFileName();
+
+            Logger.logInfo("Created temp file: " + this.FileName);
+        }
+
+        #endregion
+
+        #region Delegates
+
+        public delegate void ClosedEventHandler(object sender, EventArgs e);
+
+        #endregion
+
+        #region Events
+
+        public event ClosedEventHandler Closed;
+
+        #endregion
+
+        #region Properties
+
+        public bool IsStopped { get; set; }
+
+        public string FileName { get; }
+
+        public FilterParams FilterParams { get; }
+
+        public IList<int> LastLinesHistoryList { get; } = new List<int>();
+
+        public LogWindow LogWindow { get; }
+
+        public LogWindow OwnLogWindow { get; set; }
+
+        #endregion
+
+        #region Public methods
+
+        public void OpenFile()
+        {
+            FileStream fStream = new FileStream(this.FileName, FileMode.Append, FileAccess.Write, FileShare.Read);
+            this.writer = new StreamWriter(fStream, new UnicodeEncoding(false, false));
+        }
+
+        public void CloseFile()
+        {
+            if (this.writer != null)
+            {
+                this.writer.Close();
+                this.writer = null;
+            }
+        }
+
+        public bool WriteToPipe(ILogLine textLine, int orgLineNum)
+        {
             try
             {
-              this.writer.WriteLine(textLine.FullLine);
-              this.lineMappingList.Add(orgLineNum);
-              return true;
+                lock (this.FileName)
+                {
+                    lock (this.lineMappingList)
+                    {
+                        try
+                        {
+                            this.writer.WriteLine(textLine.FullLine);
+                            this.lineMappingList.Add(orgLineNum);
+                            return true;
+                        }
+                        catch (IOException e)
+                        {
+                            Logger.logError("writeToPipe(): " + e.ToString());
+                            return false;
+                        }
+                    }
+                }
             }
-            catch (IOException e)
+            catch (IOException)
             {
-              Logger.logError("writeToPipe(): " + e.ToString());
-              return false;
+                Logger.logError("writeToPipe(): file was closed: " + this.FileName);
+                return false;
             }
-          }
         }
-      }
-      catch (IOException) 
-      {
-        Logger.logError("writeToPipe(): file was closed: " + this.fileName);
-        return false;
-      }
-    }
 
-    public string FileName
-    {
-      get { return fileName; }
-    }
-
-    public FilterParams FilterParams
-    {
-      get { return this.filterParams; }
-    }
-
-    public IList<int> LastLinesHistoryList
-    {
-      get { return this.lastLinesHistoryList; }
-    }
-
-    public int GetOriginalLineNum(int lineNum)
-    {
-      lock (this.lineMappingList)
-      {
-        if (this.lineMappingList.Count > lineNum)
-          return this.lineMappingList[lineNum];
-        else
-          return -1;
-      }
-    }
-
-    public LogWindow LogWindow
-    {
-      get { return this.logWindow; }
-    }
-
-    public LogWindow OwnLogWindow
-    {
-      get { return this.ownLogWindow; }
-      set { this.ownLogWindow = value; }
-    }
-
-    public void ShiftLineNums(int offset)
-    {
-      Logger.logDebug("FilterPipe.ShiftLineNums() offset=" + offset);
-      List<int> newList = new List<int>();
-      lock (this.lineMappingList)
-      {
-        foreach (int lineNum in this.lineMappingList)
+        public int GetOriginalLineNum(int lineNum)
         {
-          int line = lineNum - offset;
-          if (line >= 0)
-          {
-            newList.Add(line);
-          }
-          else
-          {
-            newList.Add(-1);
-          }
+            lock (this.lineMappingList)
+            {
+                if (this.lineMappingList.Count > lineNum)
+                {
+                    return this.lineMappingList[lineNum];
+                }
+                else
+                {
+                    return -1;
+                }
+            }
         }
-        this.lineMappingList = newList;
-      }
-    }
 
-    public void ClearLineNums()
-    {
-      Logger.logDebug("FilterPipe.ClearLineNums()");
-      lock (this.lineMappingList)
-      {
-        for (int i = 0; i < this.lineMappingList.Count; ++i)
+        public void ShiftLineNums(int offset)
         {
-          this.lineMappingList[i] = -1;
+            Logger.logDebug("FilterPipe.ShiftLineNums() offset=" + offset);
+            List<int> newList = new List<int>();
+            lock (this.lineMappingList)
+            {
+                foreach (int lineNum in this.lineMappingList)
+                {
+                    int line = lineNum - offset;
+                    if (line >= 0)
+                    {
+                        newList.Add(line);
+                    }
+                    else
+                    {
+                        newList.Add(-1);
+                    }
+                }
+                this.lineMappingList = newList;
+            }
         }
-      }
+
+        public void ClearLineNums()
+        {
+            Logger.logDebug("FilterPipe.ClearLineNums()");
+            lock (this.lineMappingList)
+            {
+                for (int i = 0; i < this.lineMappingList.Count; ++i)
+                {
+                    this.lineMappingList[i] = -1;
+                }
+            }
+        }
+
+        public void ClearLineList()
+        {
+            lock (this.lineMappingList)
+            {
+                this.lineMappingList.Clear();
+            }
+        }
+
+        public void RecreateTempFile()
+        {
+            lock (this.lineMappingList)
+            {
+                this.lineMappingList = new List<int>();
+            }
+            lock (this.FileName)
+            {
+                CloseFile();
+                // trunc file
+                FileStream fStream = new FileStream(this.FileName, FileMode.Truncate, FileAccess.Write, FileShare.Read);
+                fStream.SetLength(0);
+                fStream.Close();
+            }
+        }
+
+        public void CloseAndDisconnect()
+        {
+            ClearLineList();
+            OnClosed();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void OnClosed()
+        {
+            if (Closed != null)
+            {
+                Closed(this, new EventArgs());
+            }
+        }
+
+        #endregion
     }
-
-    public void ClearLineList()
-    {
-      lock (this.lineMappingList)
-      {
-        this.lineMappingList.Clear();
-      }
-    }
-
-    public void RecreateTempFile()
-    {
-      lock (this.lineMappingList)
-      {
-        this.lineMappingList = new List<int>();
-      }
-      lock (this.FileName)
-      {
-        CloseFile();
-        // trunc file
-        FileStream fStream = new FileStream(this.fileName, FileMode.Truncate, FileAccess.Write, FileShare.Read);
-        fStream.SetLength(0);
-        fStream.Close();
-      }
-    }
-
-    public void CloseAndDisconnect()
-    {
-      ClearLineList();
-      OnClosed();
-    }
-
-
-    public delegate void ClosedEventHandler(object sender, EventArgs e);
-    public event ClosedEventHandler Closed;
-    private void OnClosed()
-    {
-      if (Closed != null)
-        Closed(this, new EventArgs());
-    }
-
-
-  }
 }
