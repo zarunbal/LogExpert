8f30371378bf6731bd4c5699a08d6f57418d8bcc
Zarunbal <master.of.nothing@gmx.de>
2018-03-14 17:05:20 +0100

    renamed assembly title to jsoncolumnizer and changed guid

--- src/JsonColumnizer/Properties/AssemblyInfo.cs
+++ src/JsonColumnizer/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("CsvColumnizer")]
+[assembly: AssemblyTitle("JsonColumnizer")]
 [assembly: AssemblyDescription("")]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid("d939a82b-591f-4403-9c9b-7d1f54043019")]
+[assembly: Guid("4C0B11AE-BCAF-492C-AFE7-53C9B79148BE")]
