ae5308d9112f8079f58c2f9f3a5317864fc018f6
Hirogen <hirogenat@gmail.com>
2019-03-05 10:16:31 +0100

    Update HilightDialog.cs
    * expression properties
    * removed unnecessary object initialization

--- src/LogExpert/Dialogs/HilightDialog.cs
+++ src/LogExpert/Dialogs/HilightDialog.cs
@@ -42,18 +42,15 @@ namespace LogExpert.Dialogs
 
         public List<HilightGroup> HilightGroupList
         {
-            get { return _hilightGroupList; }
-            set { _hilightGroupList = ObjectClone.Clone<List<HilightGroup>>(value); }
+            get => _hilightGroupList;
+            set => _hilightGroupList = ObjectClone.Clone<List<HilightGroup>>(value);
         }
 
         public IList<IKeywordAction> KeywordActionList { get; set; }
 
         public string PreSelectedGroupName { get; set; } = null;
 
-        private bool IsDirty
-        {
-            get { return applyButton.Image == _applyButtonImage; }
-        }
+        private bool IsDirty => applyButton.Image == _applyButtonImage;
 
         #endregion
 
@@ -529,7 +526,7 @@ namespace LogExpert.Dialogs
                              0;
                 if (!uniqueName)
                 {
-                    name = string.Format("{0} #{1}", baseName, i++);
+                    name = $"{baseName} #{i++}";
                 }
             }
 
@@ -544,7 +541,7 @@ namespace LogExpert.Dialogs
             if (groupComboBox.SelectedIndex >= 0 && groupComboBox.SelectedIndex < HilightGroupList.Count)
             {
                 HilightGroup newGroup =
-                    ObjectClone.Clone<HilightGroup>(HilightGroupList[groupComboBox.SelectedIndex]);
+                    ObjectClone.Clone(HilightGroupList[groupComboBox.SelectedIndex]);
                 newGroup.GroupName = "Copy of " + newGroup.GroupName;
                 HilightGroupList.Add(newGroup);
                 FillGroupComboBox();
