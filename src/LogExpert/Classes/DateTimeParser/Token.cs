311def2333b689adf51cc1c5da8f12d7e7e12e9c
Francois Botha <igitur@gmail.com>
2019-03-20 15:03:10 +0200

    Use thread's date format instead of manually specifying formats. Implement simple date format parser in order to split date format into parts.

---
+++ src/LogExpert/Classes/DateTimeParser/Token.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LogExpert
+{
+    internal static class Token
+    {
+        public static bool IsDatePart(string token)
+        {
+            return
+                token.StartsWith("y", StringComparison.OrdinalIgnoreCase) ||
+                token.StartsWith("m", StringComparison.OrdinalIgnoreCase) ||
+                token.StartsWith("d", StringComparison.OrdinalIgnoreCase) ||
+                token.StartsWith("s", StringComparison.OrdinalIgnoreCase) ||
+                token.StartsWith("h", StringComparison.OrdinalIgnoreCase) ||
+                string.Compare(token, "tt", StringComparison.OrdinalIgnoreCase) == 0;
+        }
+    }
+}
