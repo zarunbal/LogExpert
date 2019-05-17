6222f0d9f0085827e3b46d0aae9fee7c3cc8510b
Francois Botha <igitur@gmail.com>
2019-05-17 19:48:50 +0200

    Factor out DateFormatPartAdjuster

---
+++ src/LogExpert/Classes/DateTimeParser/DateFormatPartAdjuster.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogExpert
+{
+    // Ensures we have constant width (number of characters) date formats
+    internal static class DateFormatPartAdjuster
+    {
+        private static IDictionary<string, string> dateTimePartReplacements = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        {
+            ["y"] = "yyy",
+            ["yyy"] = "yyyy",
+            ["m"] = "mm",
+            ["d"] = "dd",
+            ["h"] = "hh",
+            ["s"] = "ss"
+        };
+
+        public static string AdjustDateTimeFormatPart(string part)
+        {
+            if (!dateTimePartReplacements.TryGetValue(part, out string adjustedPart))
+                return part;
+
+            if (char.IsUpper(part[0]))
+                return adjustedPart.ToUpper();
+            else
+                return adjustedPart.ToLower();
+        }
+
+        public static IEnumerable<string> AdjustDateTimeFormatParts(params string[] parts)
+        {
+            foreach (var part in parts)
+                yield return AdjustDateTimeFormatPart(part);
+        }
+    }
+}
