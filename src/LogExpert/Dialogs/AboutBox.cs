5beae6a75bd1a26f197e85da702a158835a9e165
Patrick Bruner <hirogenat@gmail.com>
2020-04-30 10:38:14 +0200

    changed url in the about window

--- src/LogExpert/Dialogs/AboutBox.cs
+++ src/LogExpert/Dialogs/AboutBox.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
 using System.Reflection;
 using System.Windows.Forms;
 
@@ -22,21 +19,25 @@ namespace LogExpert.Dialogs
             InitializeComponent();
             _assembly = Assembly.GetExecutingAssembly();
 
-            this.Text = string.Format("About {0}", AssemblyTitle);
-            this.labelProductName.Text = AssemblyProduct;
-            this.labelVersion.Text = AssemblyVersion;
-            this.labelCopyright.Text = AssemblyCopyright;
+            Text = $@"About {AssemblyTitle}";
+            labelProductName.Text = AssemblyProduct;
+            labelVersion.Text = AssemblyVersion;
+            labelCopyright.Text = AssemblyCopyright;
             //this.labelCompanyName.Text = AssemblyCompany;
-            this.textBoxDescription.Text = AssemblyDescription +
-                                           "\r\n\r\nCredits:\r\n\r\n" +
-                                           "DockPanel control (c) 2007 Weifen Luo \r\n" +
-                                           "Early bird test: Mathias Dräger\r\n" +
-                                           "\r\n" +
-                                           "LogExpert uses modules from:\r\n" +
-                                           "http://sourceforge.net/projects/dockpanelsuite/\r\n" +
-                                           "http://www.xml-rpc.net/";
-            string link = "http://www.log-expert.de/";
-            this.linkLabel1.Links.Add(new LinkLabel.Link(0, link.Length, link));
+            textBoxDescription.Text = AssemblyDescription +
+@"
+
+Credits:
+
+DockPanel control (c) 2007 Weifen Luo 
+Early bird test: Mathias Dräger
+
+LogExpert uses modules from:
+http://sourceforge.net/projects/dockpanelsuite/
+http://www.xml-rpc.net/";
+
+            string link = "https://github.com/zarunbal/LogExpert/";
+            linkLabelURL.Links.Add(new LinkLabel.Link(0, link.Length, link));
         }
 
         #endregion
@@ -47,12 +48,11 @@ namespace LogExpert.Dialogs
         {
             get
             {
-                object[] attributes = _assembly
-                    .GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                object[] attributes = _assembly.GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
                 if (attributes.Length > 0)
                 {
                     AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute) attributes[0];
-                    if (titleAttribute.Title != "")
+                    if (titleAttribute.Title != string.Empty)
                     {
                         return titleAttribute.Title;
                     }
@@ -67,11 +67,10 @@ namespace LogExpert.Dialogs
         {
             get
             {
-                object[] attributes = _assembly
-                    .GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+                object[] attributes = _assembly.GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
                 if (attributes.Length == 0)
                 {
-                    return "";
+                    return string.Empty;
                 }
                 return ((AssemblyDescriptionAttribute) attributes[0]).Description;
             }
@@ -81,11 +80,10 @@ namespace LogExpert.Dialogs
         {
             get
             {
-                object[] attributes = _assembly
-                    .GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                object[] attributes = _assembly.GetCustomAttributes(typeof(AssemblyProductAttribute), false);
                 if (attributes.Length == 0)
                 {
-                    return "";
+                    return string.Empty;
                 }
                 return ((AssemblyProductAttribute) attributes[0]).Product;
             }
@@ -95,11 +93,10 @@ namespace LogExpert.Dialogs
         {
             get
             {
-                object[] attributes = _assembly
-                    .GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+                object[] attributes = _assembly.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
                 if (attributes.Length == 0)
                 {
-                    return "";
+                    return string.Empty;
                 }
                 return ((AssemblyCopyrightAttribute) attributes[0]).Copyright;
             }
@@ -109,16 +106,10 @@ namespace LogExpert.Dialogs
 
         #region Events handler
 
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        private void linkLabelURL_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             string target = e.Link.LinkData as string;
             System.Diagnostics.Process.Start(target);
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            SystemInfo info = new SystemInfo();
-            MessageBox.Show(info.Info, "System info");
         }
 
         #endregion
