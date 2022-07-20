1adf498391e7874b64c0d213a4c1ab7a5fc06baa
Hirogen <hirogenat@gmail.com>
2022-07-20 08:45:02 +0200

    Version 1.9.0

--- src/Solution Items/AssemblyVersion.cs
+++ src/Solution Items/AssemblyVersion.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("LogExpert")]
 [assembly: AssemblyCopyright("Original work Copyright (c) 2008-2011 Hagen Raab\r\nModified work Copyright (c) 2022 Zarunbal|Hirogen")]
 
-[assembly: AssemblyVersion("1.8.8")]
-[assembly: AssemblyFileVersion("1.8.8")]
-[assembly: AssemblyInformationalVersion("1.8.8")]
+[assembly: AssemblyVersion("1.9.0")]
+[assembly: AssemblyFileVersion("1.9.0")]
+[assembly: AssemblyInformationalVersion("1.9.0")]
 
 [assembly: ComVisible(false)]
