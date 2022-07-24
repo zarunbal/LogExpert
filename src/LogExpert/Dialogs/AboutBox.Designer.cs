8d06fc7421d83f1c4d8d95fdf797f9b3e164c224
Hirogen <hirogenat@gmail.com>
2022-07-24 15:47:52 +0200

    prettyfy and added new 32 pixel uparrow

--- src/LogExpert/Dialogs/AboutBox.Designer.cs
+++ src/LogExpert/Dialogs/AboutBox.Designer.cs
@@ -175,7 +175,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutBox";
-            this.Padding = new System.Windows.Forms.Padding(14, 14, 14, 14);
+            this.Padding = new System.Windows.Forms.Padding(14);
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
