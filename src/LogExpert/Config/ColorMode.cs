adbcd03a61e3ecad805c3004e00fa5b65ab9f62b
ROUZIES Jean-Laurent <jrouzies@mantu.com>
2022-07-28 10:40:12 +0200

    Small code cleaning

--- src/LogExpert/Config/ColorMode.cs
+++ src/LogExpert/Config/ColorMode.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace LogExpert.Config
 {
@@ -11,31 +12,31 @@ namespace LogExpert.Config
     {
         // Bright Theme
         // https://paletton.com/#uid=15-0u0k00sH00kJ0pq+00RL00RL
-        private static System.Drawing.Color BrightBookmarkDefaultSystemColor = System.Drawing.SystemColors.Control; // Important: only supports SystemColors
-        private static System.Drawing.Color LessBrightBackgroundColor = System.Drawing.Color.FromArgb(208, 205, 206);
-        private static System.Drawing.Color BrightBackgroundColor = System.Drawing.Color.FromArgb(221, 221, 221);
-        private static System.Drawing.Color BrighterBackgroundColor = System.Drawing.Color.FromArgb(253, 253, 253);
-        private static System.Drawing.Color BrightForeColor = System.Drawing.Color.FromArgb(0, 0, 0);
+        private static readonly Color BrightBookmarkDefaultSystemColor = SystemColors.Control; // Important: only supports SystemColors
+        private static readonly Color LessBrightBackgroundColor = Color.FromArgb(208, 205, 206);
+        private static readonly Color BrightBackgroundColor = Color.FromArgb(221, 221, 221);
+        private static readonly Color BrighterBackgroundColor = Color.FromArgb(253, 253, 253);
+        private static readonly Color BrightForeColor = Color.FromArgb(0, 0, 0);
 
         // Dark Theme
         // https://paletton.com/#uid=15-0u0k005U0670008J003Y003Y
-        private static System.Drawing.Color DarkBookmarkDefaultSystemColor = System.Drawing.SystemColors.ControlDarkDark; // Important: only supports SystemColors
-        private static System.Drawing.Color LessLessDarkBackgroundColor = System.Drawing.Color.FromArgb(90, 90, 90);
-        private static System.Drawing.Color LessDarkBackgroundColor = System.Drawing.Color.FromArgb(67, 67, 67);
-        private static System.Drawing.Color DarkBackgroundColor = System.Drawing.Color.FromArgb(45, 45, 45);
-        private static System.Drawing.Color DarkerBackgroundColor = System.Drawing.Color.FromArgb(30, 30, 30);
-        private static System.Drawing.Color DarkForeColor = System.Drawing.Color.FromArgb(255, 255, 255);
+        private static readonly Color DarkBookmarkDefaultSystemColor = SystemColors.ControlDarkDark; // Important: only supports SystemColors
+        private static readonly Color LessLessDarkBackgroundColor = Color.FromArgb(90, 90, 90);
+        private static readonly Color LessDarkBackgroundColor = Color.FromArgb(67, 67, 67);
+        private static readonly Color DarkBackgroundColor = Color.FromArgb(45, 45, 45);
+        private static readonly Color DarkerBackgroundColor = Color.FromArgb(30, 30, 30);
+        private static readonly Color DarkForeColor = Color.FromArgb(255, 255, 255);
 
         // Default
-        public static System.Drawing.Color BackgroundColor = LessBrightBackgroundColor;
-        public static System.Drawing.Color DockBackgroundColor = BrighterBackgroundColor;
-        public static System.Drawing.Color BookmarksDefaultBackgroundColor = BrightBookmarkDefaultSystemColor;
-        public static System.Drawing.Color ForeColor = BrightForeColor;
-        public static System.Drawing.Color MenuBackgroundColor = BrighterBackgroundColor;
-        public static System.Drawing.Color HoverMenuBackgroundColor = LessBrightBackgroundColor;
-        public static System.Drawing.Color ActiveTabColor = BrighterBackgroundColor;
-        public static System.Drawing.Color InactiveTabColor = LessBrightBackgroundColor;
-        public static System.Drawing.Color TabsBackgroundStripColor = LessBrightBackgroundColor;
+        public static Color BackgroundColor = LessBrightBackgroundColor;
+        public static Color DockBackgroundColor = BrighterBackgroundColor;
+        public static Color BookmarksDefaultBackgroundColor = BrightBookmarkDefaultSystemColor;
+        public static Color ForeColor = BrightForeColor;
+        public static Color MenuBackgroundColor = BrighterBackgroundColor;
+        public static Color HoverMenuBackgroundColor = LessBrightBackgroundColor;
+        public static Color ActiveTabColor = BrighterBackgroundColor;
+        public static Color InactiveTabColor = LessBrightBackgroundColor;
+        public static Color TabsBackgroundStripColor = LessBrightBackgroundColor;
 
 
         public static bool DarkModeEnabled = false;
