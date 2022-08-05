b1de96ad5df7abef3003b5f282f6f64c3e2d6780
Matthias Schmidt <schmidt.matthias93@outlook.com>
2022-08-05 11:09:09 +0200

    create context menu entry

--- chocolatey/tools/chocolateyuninstall.ps1
+++ chocolatey/tools/chocolateyuninstall.ps1
@@ -4,3 +4,6 @@ $applicationName = "LogExpert"
 
 # remove start menu shortcut
 Remove-Item "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\$applicationName.lnk"
+
+# remote context menu entry
+Remove-Item -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\*\shell\$applicationName" -Recurse
