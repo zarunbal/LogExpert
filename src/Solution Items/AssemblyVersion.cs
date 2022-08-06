907daf68b90f754c6f7e07bcbf193ae1fa53b20d
Hirogen <hirogenat@gmail.com>
2022-08-06 09:50:15 +0200

    9001 testrelease

--- src/Solution Items/AssemblyVersion.cs
+++ src/Solution Items/AssemblyVersion.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("LogExpert")]
 [assembly: AssemblyCopyright("Original work Copyright (c) 2008-2011 Hagen Raab\r\nModified work Copyright (c) 2022 Zarunbal|Hirogen")]
 
-[assembly: AssemblyVersion("1.9.0")]
-[assembly: AssemblyFileVersion("1.9.0")]
-[assembly: AssemblyInformationalVersion("1.9.0")]
+[assembly: AssemblyVersion("1.9.1.9001")]
+[assembly: AssemblyFileVersion("1.9.1.9001")]
+[assembly: AssemblyInformationalVersion("1.9.1 Testrelease")]
 
 [assembly: ComVisible(false)]
