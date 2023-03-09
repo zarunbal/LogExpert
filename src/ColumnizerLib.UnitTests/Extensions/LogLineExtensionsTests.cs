396ff5e4f05d5414c672d88274a9b4bfc8491400
Mike Perrin <mike@alvo.co.uk>
2023-03-09 15:07:19 +0000

    copy to clipboard: always add line number

---
+++ src/ColumnizerLib.UnitTests/Extensions/LogLineExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using LogExpert;
+using NUnit.Framework;
+
+namespace ColumnizerLib.UnitTests.Extensions
+{
+    [TestFixture]
+
+    internal class LogLineExtensionsTests
+    {
+        private class TestingLogLine : ILogLine
+        {
+            public string FullLine { get; set; }
+
+            public int LineNumber { get; set; }
+
+            public string Text { get; set; }
+        }
+
+        [Test]
+        public void ToClipBoardText_ReturnsExpected()
+        {
+            var underTest = new TestingLogLine
+            {
+                FullLine = "a fullLine",
+                LineNumber = 89,
+                Text = "a text"
+            };
+            Assert.AreEqual("\t90\ta fullLine", underTest.ToClipBoardText());
+        }
+    }
+}
