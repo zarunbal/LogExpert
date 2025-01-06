2113c9b17bf3440f526e5b7b416fb2df3e7cd069
Hirogen <hirogenat@gmail.com>
2025-01-06 10:46:39 +0100

    update assembly version for .net 8 pre release

--- src/Solution Items/AssemblyVersion.cs
+++ src/Solution Items/AssemblyVersion.cs
@@ -3,10 +3,10 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("LogExpert")]
-[assembly: AssemblyCopyright("Original work Copyright (c) 2008-2011 Hagen Raab\r\nModified work Copyright (c) 2022 Zarunbal|Hirogen")]
+[assembly: AssemblyCopyright("Original work Copyright (c) 2008-2011 Hagen Raab\r\nModified work Copyright (c) 2025 Zarunbal|Hirogen and many others")]
 
-[assembly: AssemblyVersion("1.9.0")]
-[assembly: AssemblyFileVersion("1.9.0")]
-[assembly: AssemblyInformationalVersion("1.9.0")]
+[assembly: AssemblyVersion("1.10.0")]
+[assembly: AssemblyFileVersion("1.10.0")]
+[assembly: AssemblyInformationalVersion("1.10.0")]
 
 [assembly: ComVisible(false)]
