9f0520a98aa8c5e9320ddb44f990cbec2688ad2e
Hirogen <hirogenat@gmail.com>
2026-03-03 20:27:21 +0100

    update gitversion

--- src/Solution Items/AssemblyInfo.cs
+++ src/Solution Items/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("LogExpert")]
 [assembly: AssemblyCopyright("Original work Copyright (c) 2008-2011 Hagen Raab\r\nModified work Copyright (c) 2024-2026 Zarunbal|Hirogen and many others")]
 
-[assembly: AssemblyVersion("1.21.0")]
-[assembly: AssemblyFileVersion("1.21.0")]
-[assembly: AssemblyInformationalVersion("1.21.0 Release")]
+[assembly: AssemblyVersion("1.30.0")]
+[assembly: AssemblyFileVersion("1.30.0")]
+[assembly: AssemblyInformationalVersion("1.30.0 Release")]
 
 [assembly: ComVisible(false)]
 //warning CA1824: Mark assemblies with NeutralResourcesLanguageAttribute
